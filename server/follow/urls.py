00b389860c99477c484232353b0560da46725894
hausdorff <centralrook7@gmail.com>
2022-09-13 15:45:14 +0900

    add follow_user.html

--- server/follow/urls.py
+++ server/follow/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='account_login'),
     path('logout/', views.LogoutView.as_view(), name='account_logout'),
     path('signup/', views.SignupView.as_view(), name='account_signup'),
+    path('follow_user/', views.SignupView.as_view(), name='follow_user'),
 ]
