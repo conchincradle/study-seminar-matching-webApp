6b7aaa515081989faca564d9fa37c188086338ec
zhu <1439208355@qq.com>
2022-09-14 09:07:13 +0900

    add mypage, userpage, profile

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='account_login'),
     path('logout/', views.LogoutView.as_view(), name='account_logout'),
     path('signup/', views.SignupView.as_view(), name='account_signup'),
+    path('mypage/',views.mypage, name='account_mypage'),
+    path('userpage/',views.userpage, name='account_userpage'),
 ]
--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from allauth.account import views
+from django.utils import timezone
 
 class LoginView(views.LoginView):
     template_name = 'accounts/login.html'
@@ -14,3 +15,43 @@ class LogoutView(views.LogoutView):
 
 class SignupView(views.SignupView):
     template_name = 'accounts/signup.html'
+
+# mypage test-Zhu
+def mypage(request):
+    context = {
+            'user_icon':"https://iconutopia.com/wp-content/uploads/2016/06/space-dog-laika1.png",
+            'user_name': 'ラブパイ',
+            "profile":'''私はプログラミングに関するスキルの幅広さに自信があります。C言語やPHPはもちろん、Rubyに関しても知見がありますので、
+                      幅広い業務に対応できる強みがあります。また、現在はScalaを独学で学んでおり、
+                      今後も状況に応じて必要性の高いプログラミング言語を中心に学んでいきたいと考えております。''',
+            "sound_profile":"",
+            "birthday":timezone.now(),
+
+            }
+    return render(request,'accounts/mypage.html',context)
+
+def userpage(request):
+    context = {
+            'user_icon':"https://iconutopia.com/wp-content/uploads/2016/06/space-dog-laika1.png",
+            'user_name': 'ラブパイ',
+            "profile":'''私はプログラミングに関するスキルの幅広さに自信があります。C言語やPHPはもちろん、Rubyに関しても知見がありますので、
+                      幅広い業務に対応できる強みがあります。また、現在はScalaを独学で学んでおり、
+                      今後も状況に応じて必要性の高いプログラミング言語を中心に学んでいきたいと考えております。''',
+            "sound_profile":"",
+            "birthday":timezone.now(),
+
+            }
+    return render(request,'accounts/userpage.html',context)
+
+from django.core.files.base import ContentFile
+
+
+# test
+
+def save_file(request):
+    mymodel = MyModel.objects.get(id=1)
+    # 读取上传的文件中的video项为二进制文件
+    file_content = ContentFile(request.FILES['video'].read())
+    # ImageField的save方法，第一个参数是保存的文件名，第二个参数是ContentFile对象，里面的内容是要上传的图片、视频的二进制内容
+    mymodel.video.save(request.FILES['video'].name, file_content)
+###### test zhu
