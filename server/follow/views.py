69748e8453ce9bc32ecfa73eb23ef2e8989a3d93
yudaiOfRiver <ykawa412@gmail.com>
2022-09-14 14:25:45 +0900

    user_idをフロントに渡す

--- server/follow/views.py
+++ server/follow/views.py
@@ -12,10 +12,10 @@ class followings(View):
         target_user = AccountUser.objects.get(id=self.kwargs['user_id'])
         relations = FollowRelation(user=target_user)
         followings = relations.following.all()
-        return render(request, "follow/followings.html", {'user_name': target_user.user_name, 'followings': followings})
+        return render(request, "follow/followings.html", {'user_name': target_user.user_name,'user_id':target_user.id, 'followings': followings})
 
 class followers(View):
     def get(self, request, *args, **kwargs):
         target_user = AccountUser.objects.get(id=self.kwargs['user_id'])
         followers = target_user.followed_by.all()
-        return render(request, "follow/followers.html", {'user_name': target_user.user_name,'followers': followers})
+        return render(request, "follow/followers.html", {'user_name': target_user.user_name,'user_id':target_user.id, 'followers': followers})
