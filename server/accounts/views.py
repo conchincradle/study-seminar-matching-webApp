3ecce521cd00be2d24297369a3c885b0bae0a4e2
zhu <1439208355@qq.com>
2022-09-15 11:49:35 +0900

    commit for accounts

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -67,18 +67,6 @@ class MypageView(LoginRequiredMixin, View):
 
         print(type(AccountUser.user_name))
         print(AccountUser.objects.values())
-        # create AccountUser for the current user
-        '''
-         if not AccountUser.objects.filter(user_name=user.pk):
-            user_icon_default = "https://iconutopia.com/wp-content/uploads/2016/06/space-dog-laika1.png"
-            sound_profile_default = "https://www.3anet.co.jp/np/secure/0-0001-01-333020/0-0001-01-333020-1.exp/001.mp3"
-            profile_default = "プロファイル編集お願いします！"
-            birthday_default = timezone.now().date()
-            account = AccountUser(user_name=user.pk, brthday=birthday_default, user_icon=user_icon_default, sound_profile=sound_profile_default, profile=profile_default)
-            account.save()
-        
-        '''
-
         print("-------------------------------")
         print(AccountUser.objects.values())
         post_data = AccountUser.objects.get(user_name=user.pk)
@@ -96,6 +84,8 @@ class MypageView(LoginRequiredMixin, View):
         print(post_data.user_icon)
 
         return render(request, 'accounts/mypage.html', context)
+
+    
     
     def post(self, request, *args, **kwargs):
         form = ProfileEditForm(request.POST or None)
@@ -112,6 +102,8 @@ class MypageView(LoginRequiredMixin, View):
         return render(request, 'accounts/post_form.html', {
             'form':form
         })      
+        
+
 # mypage test-Zhu
 class PlanView(LoginRequiredMixin, View):
     def get(self,request,*args,**kwargs):
@@ -130,16 +122,21 @@ class PlanView(LoginRequiredMixin, View):
 
 
 def userpage(request):
+    print(target_user.id)
+    post_data = AccountUser.objects.get(user_name=user.pk)
+    print(post_data)
+    sedai = self.calculate_age(post_data.birthday)
     context = {
-            'user_icon':"https://iconutopia.com/wp-content/uploads/2016/06/space-dog-laika1.png",
-            'user_name': 'ラブパイ',
-            "profile":'''私はプログラミングに関するスキルの幅広さに自信があります。C言語やPHPはもちろん、Rubyに関しても知見がありますので、
-                      幅広い業務に対応できる強みがあります。また、現在はScalaを独学で学んでおり、
-                      今後も状況に応じて必要性の高いプログラミング言語を中心に学んでいきたいと考えております。''',
-            "sound_profile":"",
-            "birthday":timezone.now(),
+        'user_name': user.username,
+        'user_icon': post_data.user_icon,
+        'profile': post_data.profile,
+        'sound_profile': post_data.sound_profile,
+        'sedai': sedai,
 
-            }
+    }
+    print(post_data.user_icon)
+
+
     return render(request,'accounts/userpage.html',context)
 
 '''
